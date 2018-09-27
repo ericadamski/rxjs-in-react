c3d12dd2ea59ad05dda5acc88b3e39c37202395f
Eric Adamski <er.adamski@gmail.com>
2018-09-27 09:12:30 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled, { injectGlobal } from "styled-components";
+
+// import fetch from 'unfetch';
 
 class App extends Component {
+  state = {
+    clicks: 0
+  };
+
+  handleClick(event) {
+    this.setState(({ clicks }) => ({ clicks: ++clicks }));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Container>
+          Click Me
+          <span role="img">↓</span>
+          <Button onClick={this.handleClick.bind(this)}>
+            <span role="img" aria-label="angel">
+              😇
+            </span>
+          </Button>
+        </Container>
+        <Clicks>Clicked the button {this.state.clicks} times.</Clicks>
+      </Container>
     );
   }
 }
 
 export default App;
+
+injectGlobal`
+  html, body {
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  background-color: #f5e6e8;
+
+  font-size: 2rem;
+`;
+
+const Clicks = styled.span`
+  font-size: 4rem;
+`;
+
+const Button = styled.button`
+  background-color: #d5c6e0;
+  color: #192a51;
+
+  height: 3rem;
+  width: 12rem;
+
+  font-size: 2rem;
+  line-height: 2rem;
+`;
